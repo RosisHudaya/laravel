2836a1c678fa982c1b2f0d7fbba53eb6fb4b194b
yasimaru00 <yasimaru00@gmail.com>
2022-12-14 23:41:33 +0700

    edit route delete --force

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,9 @@ use Illuminate\Support\Facades\Artisan;
 
 Route::get('/mysql', function () {
     // Artisan::call('migrate:rollback', ['--force' => true]);
-    Artisan::call('migrate:fresh', ['--force' => true]);
-    Artisan::call('db:seed', ['--force' => true]);
+    Artisan::call('migrate:fresh');
+    // Artisan::call('db:seed', ['--force' => true]);
+    Artisan::call('db:seed');
 });
 
 
