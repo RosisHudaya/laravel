a01c64127aa48e518ecf3e4af618364ca38c59db
yasimaru00 <yasimaru00@gmail.com>
2022-12-15 12:52:52 +0700

    fix clear route

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Artisan;
 */
 
 Route::get('/mysql', function () {
+    Artisan::call('migrate:fresh --env=production');
     Artisan::call('migrate:fresh', ['--force' => true]);
     Artisan::call('db:seed', ['--force' => true]);
 });
